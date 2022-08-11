d9608eff30c8902e7546660081b17378791d978d
sbtoonz <sbtoonz@gmail.com>
2022-08-11 05:32:27 -0700

    Version Bump

--- Trader2.0/ModCore.cs
+++ Trader2.0/ModCore.cs
@@ -15,7 +15,7 @@ namespace Trader20
     public class Trader20 : BaseUnityPlugin
     {
         private const string ModName = "KnarrTheTrader";
-        public const string ModVersion = "0.2.6";
+        public const string ModVersion = "0.2.7";
         private const string ModGUID = "com.zarboz.KnarrTheTrader";
         internal static ConfigSync configSync = new(ModGUID)
         {
