fc0d356bd511c7342b52d7ff775a8b7455301c3e
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2023-02-13 13:42:57 -0800

    remove ShaderReplacer.dll and replace with raw script

--- UnityProject/Trader2.0/Assets/Scripts/ShaderReplacer.cs
+++ UnityProject/Trader2.0/Assets/Scripts/ShaderReplacer.cs
@@ -1,17 +1,38 @@
+using System;
 using UnityEngine;
 
 public class ShaderReplacer : MonoBehaviour
 {
     [Tooltip("Use this Field For Normal Renderers")] [SerializeField]
     internal Renderer[] _renderers;
+
+    private bool _flipped = false;
     private void OnEnable()
     {
+        if (_flipped) return;
+        if(ZNetScene.m_instance.m_namedPrefabs.Count <= 0) return;
         foreach (var renderer in _renderers)
         {
             foreach (var material in renderer.sharedMaterials)
             {
                 string name = material.shader.name;
                 material.shader = Shader.Find(name);
+                _flipped = true;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        if (_flipped) return;
+        if(ZNetScene.m_instance.m_namedPrefabs.Count <= 0) return;
+        foreach (var renderer in _renderers)
+        {
+            foreach (var material in renderer.sharedMaterials)
+            {
+                string name = material.shader.name;
+                material.shader = Shader.Find(name);
+                _flipped = true;
             }
         }
     }
