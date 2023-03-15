579938c17c7ebe236abb6f62667c9e032de5edc8
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2023-03-15 10:40:26 -0700

    V0.3.4

--- Trader2.0/ModCore.cs
+++ Trader2.0/ModCore.cs
@@ -15,7 +15,7 @@ namespace Trader20
     public class Trader20 : BaseUnityPlugin
     {
         private const string ModName = "KnarrTheTrader";
-        public const string ModVersion = "0.3.3";
+        public const string ModVersion = "0.3.4";
         private const string ModGUID = "com.zarboz.KnarrTheTrader";
         internal static ConfigSync configSync = new(ModGUID)
         {
